3c8e75efd0f93f5e84a87953850eb6a4e2565b9b
Jonathan Dantas <jonathan.oliveira.da@gmail.com>
2022-06-22 23:07:59 -0300

    Exercise Beecrowd 1013.js

--- 1013.js
+++ 1013.js
@@ -1,8 +1,15 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
-
+/*
 var [A, B, C] = input.split(" ");
 
 var maiorAB = parseInt((A + B + (A - B)) / 2);
 
-console.log("TRIANGULO: " + maiorAB);
+console.log("TRIANGULO: " + maiorAB);*/
+
+const [A, B, C] =  input.split(" ").map(item => parseInt(item));
+
+const maiorAB = (A + B + Math.abs(A - B)) / 2;
+const maiorXC = (C + maiorAB + Math.abs(C - maiorAB)) / 2;
+
+console.log(maiorXC);
