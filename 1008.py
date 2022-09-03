d79f71cbe9fba18e48bec78311fb44d7ada8f6c8
Jonathan Dantas <jonathan.oliveira.da@gmail.com>
2022-09-02 22:53:29 -0300

    Exercise Beecrowd 1008

---
+++ 1008.py
@@ -0,0 +1,9 @@
+number = int(input())
+horas = int(input())
+salario = float(input())
+
+salario_final = horas * salario
+
+print('NUMBER = %.0f' % number)
+print('SALARY = U$ %.2f' % salario_final)
+
