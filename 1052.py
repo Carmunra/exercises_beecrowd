ac7f9f0136b4ffcfcff474cd949181afe7fe6844
Jonathan Dantas <89612433+Carmunra@users.noreply.github.com>
2023-01-05 22:20:24 -0300

    Exercise Beecrowd 1052.py
    Program that prints the months of the year according to the number entered.

---
+++ 1052.py
@@ -0,0 +1,31 @@
+
+mes = int(input())
+
+meses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+if mes == 1:
+    print(meses[0])
+elif mes == 2:
+    print(meses[1])
+elif mes == 3:
+    print(meses[2])
+elif mes == 4:
+    print(meses[3])
+elif mes == 5:
+    print(meses[4])
+elif mes == 6:
+    print(meses[5])
+elif mes == 7:
+    print(meses[6])
+elif mes == 8:
+    print(meses[7])
+elif mes == 9:
+    print(meses[8])
+elif mes == 10:
+    print(meses[9])
+elif mes == 11:
+    print(meses[10])
+elif mes == 12:
+    print(meses[11])
+else:
+    print('Insira um valor entre 1 e 12')
